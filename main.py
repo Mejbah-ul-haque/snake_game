c1f5a011e075d2c2b3d832b540cb4651a508cc9f
md-mejbaul-haque <mejba.s@gmail.com>
2025-07-08 00:48:00 +0600

    Scoreboard file added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from turtle import Screen
+
+from scoreboard import Scoreboard
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +14,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
